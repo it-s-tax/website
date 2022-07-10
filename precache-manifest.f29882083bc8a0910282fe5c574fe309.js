01e89c9c8e7ada783e69eb5851d086c85cee5b6a
jhyoon9705 <jhyoon513@naver.com>
2022-07-10 10:30:48 +0900

    Updates

---
+++ precache-manifest.f29882083bc8a0910282fe5c574fe309.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e9c81c9c9be03b8a030d299f6dac7b",
+    "url": "/www.itstax213.co.kr/index.html"
+  },
+  {
+    "revision": "6d463511f50e1569b3f9",
+    "url": "/www.itstax213.co.kr/static/css/2.f6fac6ca.chunk.css"
+  },
+  {
+    "revision": "ca5d7f5725dd2a783e52",
+    "url": "/www.itstax213.co.kr/static/css/main.54240357.chunk.css"
+  },
+  {
+    "revision": "6d463511f50e1569b3f9",
+    "url": "/www.itstax213.co.kr/static/js/2.00b79d1f.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/www.itstax213.co.kr/static/js/2.00b79d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5d7f5725dd2a783e52",
+    "url": "/www.itstax213.co.kr/static/js/main.7cf083df.chunk.js"
+  },
+  {
+    "revision": "f378234b33413ad1b2b9",
+    "url": "/www.itstax213.co.kr/static/js/runtime-main.e4e83776.js"
+  },
+  {
+    "revision": "91a09f92c3c245ded1fcb9546b0722ac",
+    "url": "/www.itstax213.co.kr/static/media/CEO_profile.91a09f92.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/www.itstax213.co.kr/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "567f791d3ce7c00bcfd0c96230277d12",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-01.567f791d.svg"
+  },
+  {
+    "revision": "137bb0884610ac2d7607a7b19e99c95d",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-02.137bb088.svg"
+  },
+  {
+    "revision": "e1106441d0ba2ce7166630a4d00f7ef5",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-03.e1106441.svg"
+  },
+  {
+    "revision": "57e216c07f3ccb2a392b129eb309a974",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-04.57e216c0.svg"
+  },
+  {
+    "revision": "a7b294408f34828dbbdb711668a7ec83",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-05.a7b29440.svg"
+  },
+  {
+    "revision": "58cd8de23747828e24ca775e5f5dcd59",
+    "url": "/www.itstax213.co.kr/static/media/feature-tile-icon-06.58cd8de2.svg"
+  },
+  {
+    "revision": "e8ae7653e3b63cbf7b769fdee48f4cad",
+    "url": "/www.itstax213.co.kr/static/media/illustration-section-01.e8ae7653.svg"
+  },
+  {
+    "revision": "e8da595b14dfdcac68ccfbc6e937dbd6",
+    "url": "/www.itstax213.co.kr/static/media/illustration-section-02.e8da595b.svg"
+  },
+  {
+    "revision": "cfa1f595e2ee5a3ff610f8f076a09c51",
+    "url": "/www.itstax213.co.kr/static/media/info01.cfa1f595.png"
+  },
+  {
+    "revision": "f8b6148171dc6ee78bbd921e3d6a2439",
+    "url": "/www.itstax213.co.kr/static/media/info02.f8b61481.png"
+  },
+  {
+    "revision": "248386cc1b47d2d84afe98fdf7fbe271",
+    "url": "/www.itstax213.co.kr/static/media/info03.248386cc.png"
+  },
+  {
+    "revision": "a6f79bc613ff4c3bfdfefd522ea9a86b",
+    "url": "/www.itstax213.co.kr/static/media/office01.a6f79bc6.jpg"
+  },
+  {
+    "revision": "96d3398f5de290e4bbd47298c9bc461d",
+    "url": "/www.itstax213.co.kr/static/media/office02.96d3398f.jpg"
+  },
+  {
+    "revision": "b5bb49c7a7c193d2fb7c181abdd1d088",
+    "url": "/www.itstax213.co.kr/static/media/office03.b5bb49c7.jpg"
+  },
+  {
+    "revision": "5c2321e30de60780f40934eb04d9b11d",
+    "url": "/www.itstax213.co.kr/static/media/office04.5c2321e3.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/www.itstax213.co.kr/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/www.itstax213.co.kr/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/www.itstax213.co.kr/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/www.itstax213.co.kr/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "08336f5cc07bb7b31a1c5d49ba36a65d",
+    "url": "/www.itstax213.co.kr/static/media/top_logo.08336f5c.PNG"
+  }
+]);
